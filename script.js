26b9b202ac3fbe767ee88e68522c19d3f97dc6f6
Qi Zhi <43718543+Bollywoodmama@users.noreply.github.com>
2024-05-17 12:20:50 +0000

    Created About page

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// document.getElementById('aboutBtn').addEventListener('click', function() {
+//     var aboutContent = document.getElementById('about-content');
+//     if (aboutContent.style.display === "none" || aboutContent.style.display === "") {
+//         aboutContent.style.display = "block";
+//     } else {
+//         aboutContent.style.display = "none";
+//     }
+// });
+
+
+// document.getElementById("faqBtn").addEventListener("click", function() {
+//     loadContent("timetable");
+// });
+
