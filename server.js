5f4860fb602e743a0d29d8767114c055a6eff6bc
Qi Zhi <43718543+Bollywoodmama@users.noreply.github.com>
2024-05-23 06:22:25 +0000

    created .env file to contain auth token and account SID

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const twilio = require('twilio');
+require('dotenv').config()
+
+const app = express();
+const client = twilio(process.env.ACCOUNT_SID,process.env.TWILIO_AUTH_TOKEN);
+
+
+app.use(bodyParser.json());
+
+app.post('/send-sms', (req, res) => {
+    const { phoneNumber, message } = req.body;
+
+    client.messages.create({
+        body: message,
+        to: phoneNumber,
+        from: '+12166779814'
+    }).then(() => {
+        res.send('SMS sent successfully!');
+    }).catch((err) => {
+        console.error(err);
+        res.status(500).send('Error sending SMS');
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
